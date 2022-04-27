fd486f648b741b5133b91c5dd008b924aa9cfc5c
d21emijo <d21emijo@student.his.se>
2022-04-27 15:53:03 +0200

    get gson from web

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,45 @@
+package com.example.readjsonfromfiledemo;
+
+import androidx.annotation.NonNull;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int size;
+
+    public Mountain(String name, String location, int size) {
+        this.name = name;
+        this.location = location;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name + " has an altitude of " + size + "m above " + location;
+    }
+}
